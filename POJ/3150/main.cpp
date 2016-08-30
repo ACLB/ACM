0cd299aa428b31a8398043ddcbd52bdded2f9bd6
ACLB <1160052833@qq.com>
2016-08-30 14:56:08 +0800

    update

---
+++ POJ/3150/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <string>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <algorithm>
+
+#define fread(x) freopen(x,"r",stdin)
+#define fwrite(x) freopen(x,"w",stdout)
+#define long long LL;
+
+using namespace std;
+
+const int Max = 1e6+100;
+const double Pi = acos(-1.0);
+const double eps = 1e-8;
+
+int main() {
+
+    return 0;
+}
