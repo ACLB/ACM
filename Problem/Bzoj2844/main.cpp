6304be5e070c4be4ad49300feb34d60a58452880
ACLB <1160052833@qq.com>
2016-11-13 22:15:08 +0800

    add Bzoj 2488

---
+++ Problem/Bzoj2844/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+
+const int maxn = 1e5+100;
+
+int Ga[maxn],n;
+
+void Gauss() {
+    for(int i = 29;i>=0;i--) {
+        
+    }
+}
+
+int main() {
+
+    return 0;
+}
