070a3b809d9392b9d4b8d4d45e7665b15872bb5c
ACLB <1160052833@qq.com>
2016-11-21 01:36:12 +0800

    add HDU 5307

--- Problem/HDU5307/main.cpp
+++ Problem/HDU5307/main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 typedef long long LL;
 typedef long double LD;
+const LD Pi = acos(-1.0);
 const int maxn = 101000;
 const int maxm = 110000;
 struct Complex{
@@ -28,7 +29,39 @@ struct Complex{
 LL sum[maxn];
 int lent[maxm];
 int n,len;
-LL ans[maxn];
+LL ans[maxn*2];
+void Rader(Complex y[]) {
+    for(int i = 1, j = len>>1;i<len-1;i++) {
+        if(i<j) swap(y[i],y[j]);
+        int k = len>>1;
+        while(j>=k) {
+            j-=k;
+            k>>=1;
+        }
+        if(j<k) j+=k;
+    }
+}
+void FFT(Complex y[],int op) {
+    Rader(y);
+    for(int h =  2;h<=len;h<<=1) {
+        Complex wn(cos(2*op*Pi/h),sin(2*op*Pi/h));
+        for(int i = 0;i<len;i+=h) {
+            Complex w(1,0);
+            for(int j = i;j<i+h/2;j++) {
+                Complex u = y[j];
+                Complex v = w*y[j+h/2];
+                y[j] = u+v;
+                y[j+h/2] = u-v;
+                w = w*wn;
+            }
+        }
+    }
+    if(op == -1) {
+        for(int i = 0;i<len;i++) y[i].real/=len;
+    }
+}
+
+
 int main() {
     int T;
     sum[0] = 0;
@@ -41,17 +74,32 @@ int main() {
             lent[i] +=lent[i-1];
         }
         CLR(ans,0);
-        for(int i = 1;i<n;i++) {
-            if(lent[i]==lent[i+1]) {
-                for(int j = i;lent[j] == lent[i];j++) ;
-                ans[0] += sum[j-i+1]; 
+        for(int i = 1;i<=n;i++) {
+            if(lent[i]==lent[i-1]) {
+                int j  = i;
+                for(;j<n&&lent[j+1] == lent[i];j++) ;
+                ans[0] += sum[j-i+1];
+               i = j; 
             }
         }
+        printf("%lld\n",ans[0]);
         len = 1;
-        while(len<=(2*sum[n])) len<<=1;
+        while(len<=(2*lent[n])) len<<=1;
         CLR(a,0); CLR(b,0);
-        for(int i = 1;i<=n;i++) a[sum[i]].rea+=i;
-        for(int i = 1)
+        for(int i = 1;i<=n;i++) a[lent[i]].real+=i;
+        for(int i = 1;i<=n;i++) b[lent[n]-lent[i-1]].real+=1;
+        FFT(a,1);FFT(b,1);
+        for(int i = 0;i<len;i++) a[i] = a[i]*b[i];
+        FFT(a,-1);
+        for(int i = 0;i<len;i++) ans[i] = LL(a[i].real+0.5);
+        CLR(a,0); CLR(b,0);
+        for(int i = 1;i<=n;i++) a[lent[i]].real+=1;
+        for(int i = 1;i<=n;i++) b[lent[n] - lent[i-1]].real+=(i-1);
+        FFT(a,1);FFT(b,1);
+        for(int i = 0;i<len;i++) a[i] = a[i]*b[i];
+        FFT(a,-1);
+        for(int i = 0;i<len;i++) ans[i] -= LL(a[i].real+0.5);
+        for(int i = lent[n]+1;i<=lent[n]*2;i++) printf("%lld\n",ans[i]);
     }
     return 0;
 }
